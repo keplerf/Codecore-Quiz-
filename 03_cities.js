27ad331df462eeeaf2bcf2aaaadb098ca6990ad4
keplerf <keplerweb@gmail.com>
2016-08-02 12:28:46 -0700

    Given a JavaScript object that looks like this:

--- 03_cities.js
+++ 03_cities.js
@@ -1,15 +1,11 @@
+var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]};
 
-    var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]};
+  // prints each province on its own line.
+  var printCities = function(cities) {
+    for (var city in cities) {
+      var provincialCities = cities[city].length;
+      console.log(city + " has " + provincialCities + " cities");
+    }
+  };
 
-    // prints each province on its own line.
-    var printCities = function(cities) {
-      for (var city in cities) {
-        var provincialCities = cities[city].length;
-        console.log(city + " has " + provincialCities + " cities");
-      }
-    };
-
-    printCities(majorCities);
-
-
-  
+  printCities(majorCities);
