78db0a1a3fc26ac48c5b30ba58e6648ff9191fa5
keplerf <keplerweb@gmail.com>
2016-08-02 12:34:38 -0700

    mplement the following code in Ruby: Create a module called HelperMethods that include a method called  that does the following:

--- 07_titleize.rb
+++ 07_titleize.rb
@@ -1,11 +1,10 @@
-
-  def titleize(string)
-    prepositions = ["in", "the",  "of", "and", "or" , "from"]
-    string.split(" ").map do |word|
-      if prepositions.include?(word)
-        word
-      else
-        word.capitalize
-      end
-    end.join(" ")
-  end
+def titleize(string)
+  prepositions = ["in", "the",  "of", "and", "or" , "from"]
+  string.split(" ").map do |word|
+    if prepositions.include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end.join(" ")
+end
