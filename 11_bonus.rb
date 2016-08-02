0feeaba5ab401884b6bc2140dd25d23914775685
keplerf <keplerweb@gmail.com>
2016-08-02 12:39:44 -0700

    Bonus

--- 11_bonus.rb
+++ 11_bonus.rb
@@ -1,3 +1,4 @@
+# BONUS: Rewrite the method `titleize` in question #7 without mutating any variable.
 def titleize(str)
   str.gsub(/\b(?!in)(?!the)(?!of)(?!and)(?!or)(?!from)[a-z]/){ $&.capitalize }
 end
